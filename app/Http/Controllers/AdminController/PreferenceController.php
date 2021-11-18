761954724f7fc90bbc36ac1de98892520ad17d89
Hadi-Darmawan <ghadi.darmawan@gmail.com>
2021-11-18 21:12:11 +0800

    Add Preferences

--- app/Http/Controllers/AdminController/PreferenceController.php
+++ app/Http/Controllers/AdminController/PreferenceController.php
@@ -26,8 +26,6 @@ class PreferenceController extends Controller
         $preference = Preference::first();
         $preference->nama_website_ina = $request->nama_website_ina;
         $preference->nama_website_eng = $request->nama_website_eng;
-        $preference->footer_ina = $request->footer_ina;
-        $preference->footer_eng = $request->footer_eng;
 
         if($request->logo!=""){
             $image_parts = explode(';base64', $request->logo);
@@ -41,51 +39,6 @@ class PreferenceController extends Controller
             $preference->logo_name = $filename;
             Storage::disk('public')->put($path, $image_base64);
         }
-
-        $detailina = $request->tentang_ina;
-        $detaileng = $request->tentang_eng;
-        libxml_use_internal_errors(true);
-        $dom = new \domdocument();
-        $dom->loadHtml($detailina, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-        $domeng = new \domdocument();
-        $domeng->loadHtml($detaileng, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-        $images = $dom->getElementsByTagName('img');
-        $imageseng = $domeng->getElementsByTagName('img');
-
-        foreach ($images as $count => $image) {
-            $src = $image->getAttribute('src');
-            if (preg_match('/data:image/', $src)) {
-                preg_match('/data:image\/(?<mime>.*?)\;/', $src, $groups);
-                $mimeType = $groups['mime'];
-                $path = '/image/preferences/tentang/tentang_ina/'. uniqid('', true) . '.' . $mimeType;
-                Storage::disk('public')->put($path, file_get_contents($src));
-                $image->removeAttribute('src');
-                $link = asset('storage'.$path);
-                $image->setAttribute('src', $link);
-                array_push($arrImage, $path);
-            }
-        }
-
-        foreach ($imageseng as $count => $image) {
-            $src = $image->getAttribute('src');
-            if (preg_match('/data:image/', $src)) {
-                preg_match('/data:image\/(?<mime>.*?)\;/', $src, $groups);
-                $mimeType = $groups['mime'];
-                $path = '/image/preferences/tentang/tentang_ina/'. uniqid('', true) . '.' . $mimeType;
-                Storage::disk('public')->put($path, file_get_contents($src));
-                $image->removeAttribute('src');
-                $link = asset('storage'.$path);
-                $image->setAttribute('src', $link);
-                array_push($arrImage, $path);
-            }
-        }
-
-        $detailina = $dom->savehtml();
-        $preference->tentang_ina = $detailina;
-        $detaileng = $domeng->savehtml();
-        $preference->tentang_eng = $detaileng;
-
-
         $preference->save();
         return redirect('/admin/setting/preferences')->with('statusInput', 'Preferences berhasil disimpan');
     }
